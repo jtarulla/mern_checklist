356aeb1182fbe52425b88d37e2432630fc08e43b
Javier <jtarulla@gmail.com>
2020-02-14 19:22:47 +0100

    redux connected with react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import AppNavBar from './components/AppNavBar';
 import CheckList from './components/CheckList';
+import ItemModal from './components/ItemModal';
+import { Container } from 'reactstrap';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
 	return (
-		<div className='App'>
-			<AppNavBar />
-			<CheckList />
-		</div>
+		<Provider store={store}>
+			<div className='App'>
+				<AppNavBar />
+				<Container>
+					<ItemModal />
+					<CheckList />
+				</Container>
+			</div>
+		</Provider>
 	);
 }
 
--- client/src/components/CheckList.js
+++ client/src/components/CheckList.js
@@ -1,59 +1,42 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import uuid from 'uuid';
+import { connect } from 'react-redux';
+import { getItems, deleteItem } from '../actions/itemActions';
+import PropTypes from 'prop-types';
 
 class CheckList extends Component {
-	state = {
-		items: [
-			{ id: uuid(), name: 'Finish this code' },
-			{ id: uuid(), name: 'Learn MERN stack ASAP' },
-			{ id: uuid(), name: 'Keep looking for a job' },
-			{
-				id: uuid(),
-				name: 'Never give up! Never give up! Never give up! Never give up!'
-			}
-		]
-	};
+	componentDidMount() {
+		this.props.getItems();
+	}
+
+	onDeleteClick(id) {
+		this.props.deleteItem(id);
+	}
 
 	render() {
-		const { items } = this.state;
+		const { items } = this.props.item;
+		const { isDone } = this.props;
 		return (
 			<Container>
-				<Button
-					color='dark'
-					style={{ marginBottom: '2rem' }}
-					onClick={() => {
-						const name = prompt('Enter Item');
-						if (name) {
-							this.setState(state => ({
-								items: [...state.items, { id: uuid(), name }]
-							}));
-						}
-					}}
-				>
-					Add Item
-				</Button>
-
 				<ListGroup>
 					<TransitionGroup className='check-list'>
 						{items.map(({ id, name }) => (
 							<CSSTransition key={id} timeout={500} classNames='fade'>
 								<ListGroupItem action>
+									{name}
 									<Button
 										className='remove-btn'
-										style={{ marginRight: '0.5rem' }}
+										style={{
+											marginLeft: '0.5rem',
+											float: 'right'
+										}}
 										color='btn btn-outline-danger'
 										size='sm'
-										onClick={() => {
-											this.setState(state => ({
-												items: state.items.filter(item => item.id !== id)
-											}));
-										}}
+										onClick={this.onDeleteClick.bind(this, id)}
 									>
-										&times;
+										delete
 									</Button>
-									{name}
 								</ListGroupItem>
 							</CSSTransition>
 						))}
@@ -64,4 +47,14 @@ class CheckList extends Component {
 	}
 }
 
-export default CheckList;
+CheckList.propTypes = {
+	getItems: PropTypes.func.isRequired,
+	item: PropTypes.object.isRequired,
+	isDone: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => ({
+	item: state.item
+});
+
+export default connect(mapStateToProps, { getItems, deleteItem })(CheckList);
